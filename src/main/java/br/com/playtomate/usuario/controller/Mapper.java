32fc4922a3c4497cbdf34dcec47425b44ffd7470
Helder Ardachnikoff Gomes <tomate@MacBook-Pro-de-Helder.local>
2020-06-23 19:52:26 -0300

    cryptografando senha do usuario

--- src/main/java/br/com/playtomate/usuario/controller/Mapper.java
+++ src/main/java/br/com/playtomate/usuario/controller/Mapper.java
@@ -4,23 +4,25 @@ import br.com.playtomate.usuario.database.ServiceUsuario;
 import br.com.playtomate.usuario.domain.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Mapper {
 
     Logger logger = LoggerFactory.getLogger("Mapper");
-
+    private BCryptPasswordEncoder crypt;
     private ServiceUsuario serviceUsuario;
-    public Mapper(ServiceUsuario serviceUsuario){
+    public Mapper(ServiceUsuario serviceUsuario, BCryptPasswordEncoder crypt) {
         this.serviceUsuario = serviceUsuario;
+        this.crypt = crypt;
     }
 
     public Usuario toPessoa(UsuarioDTO dto){
         Usuario usuario = Usuario.builder()
                             .login(dto.getLogin())
                             .email(dto.getEmail())
-                            .senha(dto.getSenha())
+                            .senha(crypt.encode(dto.getSenha()))
                             .nome(dto.getNome())
                             .telefone(dto.getTelefone())
                             .serviceUsuario(serviceUsuario)
